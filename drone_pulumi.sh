e370a3325547425b792fbd97ed05e97bd62c5861
naorlivne <naorlivne@gmail.com>
2021-11-04 18:13:07 +0200

    adding mention that the pulumi token is required

--- drone_pulumi.sh
+++ drone_pulumi.sh
@@ -7,12 +7,16 @@ export PULUMI_ACCESS_TOKEN=${PLUGIN_PULUMI_ACCESS_TOKEN}
 
 if [ -z "${PLUGIN_PULUMI_COMMAND}" ]
 then
-PULUMI_COMMAND="pulumi up --non-interactive --skip-preview --yes"
+  PULUMI_COMMAND="pulumi up --non-interactive --skip-preview --yes"
+else
+  PULUMI_COMMAND="${PLUGIN_PULUMI_COMMAND}"
 fi
 
 if [ -z "${PLUGIN_PULUMI_DEPENDENCIES}" ]
 then
-PULUMI_DEPENDENCIES="pip install -r requirements.txt"
+  PULUMI_DEPENDENCIES="pip install -r requirements.txt"
+else
+  PULUMI_DEPENDENCIES="${PLUGIN_PULUMI_DEPENDENCIES}"
 fi
 
 # first run the pulumi dependencies then run the pulumi command
